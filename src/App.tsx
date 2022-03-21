00e317bbcc7c8fad0562b0b945e77d1fb6f788fa
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-21 16:18:50 +0900

    fix typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-export const App = () => {
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,4 +21,6 @@ export const App = () => {
       </header>
     </div>
   );
-};
+}
+
+export default App;
