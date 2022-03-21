5892f46858f1f2f0af4cb868e0e1e5c3411386ac
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-22 03:02:10 +0900

    fix import path

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './index.css'
 import { render } from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import reportWebVitals from './reportWebVitals'
-import { SignUp } from './pages/SignUp'
-import { Login } from './pages/Login'
+import '@/index.css'
+import { Login } from '@/pages/Login'
+import { SignUp } from '@/pages/SignUp'
+import reportWebVitals from '@/reportWebVitals'
 
 const rootElement = document.getElementById('root')
 render(
