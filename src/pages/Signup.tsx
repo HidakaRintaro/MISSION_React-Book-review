49250f154bf9aca6c6e2773dd7e59b9e54bf4581
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-22 00:55:01 +0900

    サインアップの画面レイアウトを作成

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,8 +1,43 @@
 export const SignUp: React.FC = () => {
   console.log('sing up')
   return (
-    <div>
-      <p>sign up</p>
+    <div className="max-w-sm mt-4 mx-auto px-6 py-5">
+      <h1 className="text-emerald-500 text-center font-bold text-2xl">新規登録</h1>
+      <form className="mt-6">
+        <label htmlFor="userName" className="block">
+          <span className="block text-sm text-gray-700">ユーザ名</span>
+          <input
+            id="userName"
+            type="text"
+            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-emerald-500 focus:ring-emerald-500 focus:ring-1"
+          />
+        </label>
+        <label htmlFor="email" className="block mt-6">
+          <span className="block text-sm text-gray-700">メールアドレス</span>
+          <input
+            id="email"
+            type="email"
+            placeholder="you@example.com"
+            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-emerald-500 focus:ring-emerald-500 focus:ring-1"
+          />
+        </label>
+        <label htmlFor="password" className="block mt-6">
+          <span className="block text-sm text-gray-700">パスワード</span>
+          <input
+            id="password"
+            type="password"
+            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-emerald-500 focus:ring-emerald-500 focus:ring-1"
+          />
+        </label>
+        <div className="mt-6 text-right">
+          <button
+            type="submit"
+            className="bg-emerald-500 w-full hover:bg-emerald-700 px-5 py-2 text-sm rounded-md text-white"
+          >
+            新規登録
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
