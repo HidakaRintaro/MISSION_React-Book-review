1f41e58cad44e9def46ebded5e15a6c2e37dde44
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-03-22 01:48:16 +0900

    className sort

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,30 +1,30 @@
 export const Login: React.FC = () => {
   console.log('login')
   return (
-    <div className="max-w-sm mt-4 mx-auto px-6 py-5">
-      <h1 className="text-emerald-500 text-center font-bold text-2xl">ログイン</h1>
+    <div className="mx-auto mt-4 max-w-sm px-6 py-5">
+      <h1 className="text-center text-2xl font-bold text-emerald-500">ログイン</h1>
       <form className="mt-6">
-        <label htmlFor="email" className="block mt-6">
+        <label htmlFor="email" className="mt-6 block">
           <span className="block text-sm text-gray-700">メールアドレス</span>
           <input
             id="email"
             type="email"
             placeholder="you@example.com"
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-emerald-500 focus:ring-emerald-500 focus:ring-1"
+            className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder-gray-400 shadow-sm focus:border-emerald-500 focus:outline-none focus:ring-1 focus:ring-emerald-500"
           />
         </label>
-        <label htmlFor="password" className="block mt-6">
+        <label htmlFor="password" className="mt-6 block">
           <span className="block text-sm text-gray-700">パスワード</span>
           <input
             id="password"
             type="password"
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400 focus:outline-none focus:border-emerald-500 focus:ring-emerald-500 focus:ring-1"
+            className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder-gray-400 shadow-sm focus:border-emerald-500 focus:outline-none focus:ring-1 focus:ring-emerald-500"
           />
         </label>
         <div className="mt-6 text-right">
           <button
             type="submit"
-            className="bg-emerald-500 w-full hover:bg-emerald-700 px-5 py-2 text-sm rounded-md text-white"
+            className="w-full rounded-md bg-emerald-500 px-5 py-2 text-sm text-white hover:bg-emerald-700"
           >
             ログイン
           </button>
