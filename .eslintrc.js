a2fc4ac52268c118265a3cd2f135f0b6eaacd6ce
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-04-02 18:18:58 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -60,9 +60,7 @@ module.exports = {
         paths: ['src'],
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
-      typescript: {
-        project: './',
-      },
+      typescript: {},
     },
   },
 }
