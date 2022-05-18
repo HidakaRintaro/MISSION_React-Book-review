ee021c4b6fdc70db17dc4edb67962c373697027c
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-05-18 15:34:25 +0900

    良さげなメソッドを作成

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,71 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import { errorResponse } from '~/types/errorResponse'
+import { Response } from '~/types/response'
+import { statusOk } from '~/utils/statusOk'
 
+const apiBaseUrl = process.env.API_URL ?? ''
+const responseDefault = { isError: false, error: null, success: null }
 const instance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: apiBaseUrl,
   headers: { 'Content-Type': 'application/json' },
 })
+
+const get = async <T>(url: string, config?: AxiosRequestConfig): Promise<Response<T>> => {
+  const res = await instance.get(url, config)
+
+  const result: Response<T> = responseDefault
+  if (statusOk(res.status)) {
+    result.success = res.data as T
+  } else {
+    result.isError = true
+    result.error = res.data as errorResponse
+  }
+  return result
+}
+
+const post = async <T>(url: string, data?: object, config?: AxiosRequestConfig): Promise<Response<T>> => {
+  const res = await instance.post(url, data, config)
+
+  const result: Response<T> = responseDefault
+  if (statusOk(res.status)) {
+    result.success = res.data as T
+  } else {
+    result.isError = true
+    result.error = res.data as errorResponse
+  }
+  return result
+}
+
+const put = async <T>(url: string, data?: object, config?: AxiosRequestConfig): Promise<Response<T>> => {
+  const res = await instance.put(url, data, config)
+
+  const result: Response<T> = responseDefault
+  if (statusOk(res.status)) {
+    result.success = res.data as T
+  } else {
+    result.isError = true
+    result.error = res.data as errorResponse
+  }
+  return result
+}
+
+// eslint-disable-next-line no-underscore-dangle
+const _delete = async <T>(url: string, config?: AxiosRequestConfig): Promise<Response<T>> => {
+  const res = await instance.delete(url, config)
+
+  const result: Response<T> = responseDefault
+  if (statusOk(res.status)) {
+    result.success = res.data as T
+  } else {
+    result.isError = true
+    result.error = res.data as errorResponse
+  }
+  return result
+}
+
+export default {
+  get,
+  post,
+  put,
+  delete: _delete,
+}
