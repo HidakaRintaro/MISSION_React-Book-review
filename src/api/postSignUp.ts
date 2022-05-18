1d67cbc5a1a5302f0a81af8b260f2635b60faf7f
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-05-18 16:49:17 +0900

    swaggerを使った型定義に修正

--- src/api/postSignUp.ts
+++ src/api/postSignUp.ts
@@ -1,8 +1,17 @@
 import request from '~/api/request'
 import { Response } from '~/types/response'
+import { paths } from '~/api/schema'
 
-interface Res {
-  token: string
+const path = '/users' as const
+
+type Req = paths[typeof path]['post']['parameters']['body']['body']
+type Res = paths[typeof path]['post']['responses']['200']['schema']
+
+export const postSignUp = (name: string, email: string, password: string): Promise<Response<Res>> => {
+  const requestBody: Req = {
+    name,
+    email,
+    password,
+  }
+  return request.post<Res>('/users', requestBody)
 }
-export const postSignUp = (name: string, email: string, password: string): Promise<Response<Res>> =>
-  request.post<Res>('/users', { name, email, password })
