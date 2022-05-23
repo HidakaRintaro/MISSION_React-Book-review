89cad8bf9ce0d229d76e163947a2d5667d6b0809
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-05-24 02:11:20 +0900

    簡易的な新規登録の機能を作成

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,43 +1,71 @@
-import { ChangeEventHandler } from 'react'
-import { Link } from 'react-router-dom'
-import { setEmail, setName, setPassword } from '~/store/auth'
-import { useAppDispatch, useAppSelector } from '~/store/hook'
+import { unwrapResult } from '@reduxjs/toolkit'
+import { ChangeEventHandler, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
+import { signUp } from '~/store/auth'
+import { useAppDispatch } from '~/store/hook'
+
+interface IFormInput {
+  userName: string
+  email: string
+  password: string
+}
 
 export const SignUp: React.FC = () => {
   const dispatch = useAppDispatch()
-  const auth = useAppSelector((state) => state.auth)
-  const { name, email, password } = auth
+  const navigate = useNavigate()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>()
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loginError, setLoginError] = useState(false)
 
   const handleChangeName: ChangeEventHandler<HTMLInputElement> = (event) => {
-    dispatch(setName(event.target.value))
+    setName(event.target.value)
   }
   const handleChangeEmail: ChangeEventHandler<HTMLInputElement> = (event) => {
-    dispatch(setEmail(event.target.value))
+    setEmail(event.target.value)
   }
   const handleChangePassword: ChangeEventHandler<HTMLInputElement> = (event) => {
-    dispatch(setPassword(event.target.value))
+    setPassword(event.target.value)
   }
-  const handleSubmitSignUp = () => {
-    console.log({ name, email, password })
+  const handleSubmitSignUp: SubmitHandler<IFormInput> = async () => {
+    try {
+      unwrapResult(await dispatch(signUp({ name, email, password })))
+      navigate('/')
+    } catch {
+      setLoginError(true)
+    }
   }
 
   return (
     <div className="mx-auto mt-4 max-w-sm px-6 py-5">
       <h1 className="text-center text-2xl font-bold text-emerald-500">新規登録</h1>
-      <form className="mt-6" onSubmit={handleSubmitSignUp}>
+      {loginError && <div className="font-bold text-red-500">新規登録に失敗しました</div>}
+      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+      <form className="mt-6" onSubmit={handleSubmit(handleSubmitSignUp)}>
         <label htmlFor="userName" className="block">
           <span className="block text-sm text-gray-700">ユーザ名</span>
           <input
+            {...register('userName', { required: true })}
             id="userName"
             type="text"
             className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder-gray-400 shadow-sm focus:border-emerald-500 focus:outline-none focus:ring-1 focus:ring-emerald-500"
             value={name}
             onChange={handleChangeName}
           />
+          {errors.userName?.type === 'required' && (
+            <span className="text-xs text-red-500">ユーザネームがありません</span>
+          )}
         </label>
         <label htmlFor="email" className="mt-6 block">
           <span className="block text-sm text-gray-700">メールアドレス</span>
           <input
+            {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
             id="email"
             type="email"
             placeholder="you@example.com"
@@ -45,16 +73,25 @@ export const SignUp: React.FC = () => {
             value={email}
             onChange={handleChangeEmail}
           />
+          {errors.email?.type === 'pattern' && (
+            <span className="text-xs text-red-500">メールアドレスの形式が正しくありません</span>
+          )}
         </label>
         <label htmlFor="password" className="mt-6 block">
           <span className="block text-sm text-gray-700">パスワード</span>
           <input
+            {...register('password', { required: true })}
             id="password"
             type="password"
             className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder-gray-400 shadow-sm focus:border-emerald-500 focus:outline-none focus:ring-1 focus:ring-emerald-500"
             value={password}
             onChange={handleChangePassword}
           />
+          {errors.password?.type === 'required' ? (
+            <span className="text-xs text-red-500">パスワードを入力してください</span>
+          ) : (
+            <span className="text-xs text-gray-400">８文字以上で入力してください</span>
+          )}
         </label>
         <div className="mt-6 text-right">
           <button
--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,33 +1,57 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { postSignUp } from '~/api/postSignUp'
+import { AppDispatch, RootState } from '~/store'
+
+const tokenLocalStorageKey = 'API_TOKEN' as const
 
 interface authState {
-  name: string
-  email: string
-  password: string
-  token: string
+  token: string | null
 }
 
 const initialState: authState = {
-  name: '',
-  email: '',
-  password: '',
-  token: '',
+  token: localStorage.getItem(tokenLocalStorageKey),
 }
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setName: (state, action: PayloadAction<string>) => {
-      state.name = action.payload
-    },
-    setEmail: (state, action: PayloadAction<string>) => {
-      state.email = action.payload
-    },
-    setPassword: (state, action: PayloadAction<string>) => {
-      state.password = action.payload
+    setLoginToken: (state, action: PayloadAction<string | null>) => {
+      const token = action.payload
+      state.token = token
+
+      if (!token) {
+        localStorage.removeItem(tokenLocalStorageKey)
+        return
+      }
+      localStorage.setItem(tokenLocalStorageKey, token)
     },
   },
 })
 
-export const { setName, setEmail, setPassword } = authSlice.actions
+export const { setLoginToken } = authSlice.actions
+
+export const signUp = createAsyncThunk<
+  void,
+  {
+    name: string
+    email: string
+    password: string
+  },
+  { dispatch: AppDispatch; state: RootState }
+>('auth/signup', async ({ name, email, password }, thunkApi) => {
+  // MEMO: ここの良い書き方を知りたい
+  const { success, error } = await postSignUp(name, email, password)
+  try {
+    if (error) {
+      throw new Error(error.ErrorMessageJP)
+    }
+    if (success === null || success.token === undefined) {
+      throw new Error('新規登録に失敗しました')
+    }
+
+    thunkApi.dispatch(setLoginToken(success.token))
+  } catch (e) {
+    return thunkApi.rejectWithValue(e)
+  }
+})
